a16334c0c23b71e983b1f53da9fa9e17a0a60148
albyc <albertachaj@email.arizona.edu>
2014-11-12 21:28:10 -0700

    Added some more shit

---
+++ src/view/MUDView.java
@@ -0,0 +1,34 @@
+package view;
+
+import javax.swing.JFrame;
+
+/**
+ * 
+ * @author Team Alpha-Super-Awesome-Cool-Dynamite-Wolf-Squadron
+ *
+ */
+public class MUDView extends JFrame
+{
+	private static final long serialVersionUID = 7477644930151827118L;
+
+	public static void main(String [] args)
+	{
+		new MUDView();
+	} // end of method main
+	
+	/**
+	 * 
+	 */
+	public MUDView()
+	{
+		setupGUI();
+	} // end of constructor MUDView
+	
+	/**
+	 * 
+	 */
+	public void setupGUI()
+	{
+		
+	}
+}
