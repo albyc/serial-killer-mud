c3ffa9660bf5cc938a55fcc3ee6b8623b4e13a12
Alexa Zarbock <amzarbock@email.arizona.edu>
2014-11-14 14:20:51 -0700

    more stuff

--- src/Controller/Client.java
+++ src/Controller/Client.java
@@ -19,11 +19,12 @@ import java.util.List;
  */
 public class Client 
 {
-	/*List<Player> playersInRoom = new ArrayList<Player>();
+	//needs a lot of fixing
+	List<Player> playersInRoom = new ArrayList<Player>();
 	List<MOB> mobsInRoom = new ArrayList<MOB>(); 
 	SceneRoom exit = new SceneRoom(need args here);
 	RoomCollection roomCollection = new RoomCollection(need all 30 rooms here);
-	TrappingRoom desert = new TrappingRoom("Desert", "The Desert room is a trapping room, so your chance of escaping is 50/50.", playersInRoom, mobsInRoom, exit, roomCollection);*/
+	TrappingRoom desert = new TrappingRoom("Desert", "The Desert room is a trapping room, so your chance of escaping is 50/50.", playersInRoom, mobsInRoom, exit, roomCollection);
 
 
 	public void update(List<String> messages) {
@@ -31,5 +32,4 @@ public class Client
 		
 	}
 
->>>>>>> ad621af77e60b00d6a3985c759236d44855716c7
 }
