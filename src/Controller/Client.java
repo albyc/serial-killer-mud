f210e71011e511276450022289974d6bb260466c
Alexa Zarbock <amzarbock@email.arizona.edu>
2014-11-20 11:55:28 -0700

    idk

--- src/Controller/Client.java
+++ src/Controller/Client.java
@@ -21,14 +21,7 @@ import Players.*;
  */
 public class Client 
 {
-	//needs a lot of fixing - just testing to see if room abstract class/ subclass objects work
-	List<Player> playersInRoom = new ArrayList<Player>();
-	List<MOB> mobsInRoom = new ArrayList<MOB>(); 
-	RoomCollection roomCollection = new RoomCollection(null, null);
-	SceneRoom exit = new SceneRoom(null, null, null, playersInRoom, mobsInRoom, null);
-	TrappingRoom desert = new TrappingRoom("Desert", "The Desert room is a trapping room, so your chance of escaping is 50/50.", playersInRoom, mobsInRoom, exit);
-	TrappingRoom jail = new TrappingRoom("Jail", "The Jail room is a trapping room, so your chance of escaping is 50/50.", playersInRoom, mobsInRoom, exit);
-	TrappingRoom court = new TrappingRoom("Court", "The court room is a trapping room, so your chance of escaping is 50/50.", playersInRoom, mobsInRoom, exit);
+	
 	
 	private String username; // username of the client
 	private MudGUI mudGUI; 
@@ -36,6 +29,19 @@ public class Client
 	private Socket server; // connection to the server
 	private ObjectOutputStream out; // output stream
 	private ObjectInputStream in; // input stream
+	
+	//needs a lot of fixing - just testing to see if room abstract class/ subclass objects work
+		//2 rooms - mansion (start on front porch), there's a front yard where you can die
+		List<Player> playersInRoom = new ArrayList<Player>();
+		List<MOB> mobsInRoom = new ArrayList<MOB>(); 
+		MOB mob = new MOB();
+		RoomCollection roomCollection = new RoomCollection(null, null);
+		SceneRoom frontPorch = new SceneRoom("Front Porch" , "This is your initial starting location.", null, username, mob, frontYard);
+		TrappingRoom frontYard = new TrappingRoom("Front Yard", "The front yard is a trapping room, so your chance of escaping is 50/50.", null, mob, frontPorch);
+		/*SceneRoom exit = new SceneRoom(null, null, null, playersInRoom, mobsInRoom, null);
+		TrappingRoom desert = new TrappingRoom("Desert", "The Desert room is a trapping room, so your chance of escaping is 50/50.", playersInRoom, mobsInRoom, exit);
+		TrappingRoom jail = new TrappingRoom("Jail", "The Jail room is a trapping room, so your chance of escaping is 50/50.", playersInRoom, mobsInRoom, exit);
+		TrappingRoom court = new TrappingRoom("Court", "The court room is a trapping room, so your chance of escaping is 50/50.", playersInRoom, mobsInRoom, exit);*/
 	
 	public static void main (String []args)
 	{
