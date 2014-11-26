7fefaf8b86cf58d21c1bc3aa4ce65ed16dc77967
Alexa Zarbock <amzarbock@email.arizona.edu>
2014-11-25 20:26:37 -0700

    commitz

--- src/View/MainPanel.java
+++ src/View/MainPanel.java
@@ -223,6 +223,7 @@ public class MainPanel extends JPanel
 					output.writeObject(new AddChatMessageCommand(clientName + ":  " + argument));
 					break;
 				case SHUTDOWN:
+					//calling system.exit(0) here doesn't shutdown all clients - just the one that implemented shutdown
 					output.writeObject(new ForServerCommand(clientName, c));
 					//make one of the player's an admin - only they can shut down if they input their password?
 					break;
